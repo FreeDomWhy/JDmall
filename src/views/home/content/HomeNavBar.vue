acf3570d3872dd8db3826999cae8a8d98013c86d
FreeDom <1099469389@qq.com>
2021-07-27 18:13:24 +0800

    初始化项目

--- src/views/home/content/HomeNavBar.vue
+++ src/views/home/content/HomeNavBar.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="home-nav-bar">
-    <nav-bar></nav-bar>
+  <div>
+    <nav-bar class="home-nav-bar">
+      <nav-bar-cate slot="nav-bar-left"></nav-bar-cate>
+    </nav-bar>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/NavBar/NavBar";
+import NavBarCate from "views/home/common/navbar/NavBarCate";
 
 export default {
   name: "HomeNavBar",
   components:{
-    NavBar
+    NavBar,
+    NavBarCate
   }
 }
 </script>
 
 <style scoped>
 .home-nav-bar{
+  background-color: #e43130;
 }
 </style>
